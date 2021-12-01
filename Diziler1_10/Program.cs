ce008bc8a324fa265b3466a18b7913fc1e9c2c82
= <acikgoze18@itu.edu.tr>
2021-12-01 17:50:56 +0300

    Initial commit

---
+++ Diziler1_10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Diziler1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+
+            string[] hayvanlar = {"Kedi", "Kopek", "Kus", "Maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+            
+            Console.WriteLine(hayvanlar[1]);
+            Console.WriteLine(dizi[3]);
+            Console.WriteLine(renkler[0]);
+            
+            //Dongulerle dizi hesaplama
+            //Klavyeden girilen n sayinin ortalamasini hesaplayan program
+            Console.Write("Lutfen dizinin eleman sayisini giriniz: ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.WriteLine("Lutfen {0}. sayiyi girin",i+1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+                toplam += sayi;
+            
+            Console.WriteLine("Ortalama: " + toplam/diziUzunlugu);
+        }
+    }
+}
